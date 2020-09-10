9bd9e081912540995eed2820823d5116b8b84166
Couchant <37212549+Couchant@users.noreply.github.com>
2020-09-10 17:43:10 +0800

    refactor: 提取出公共entities,创建common包负责管理

--- cloud-api-commons/src/main/java/com/hallen/springcloud/entities/result/CommonResult.java
+++ cloud-api-commons/src/main/java/com/hallen/springcloud/entities/result/CommonResult.java
@@ -1,5 +1,6 @@
 package com.hallen.springcloud.entities.result;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,12 +9,13 @@ import lombok.NoArgsConstructor;
  * @ClassName: CommonResult
  * @Description: TODO
  * @Author: Hallen
- * @Date: 2020/9/2 17:55
+ * @Date: 2020/9/3 12:23
  * @Version: 1.0
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommonResult<T> {
     /**
      * 返回码 200-成功
